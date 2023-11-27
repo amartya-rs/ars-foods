223bd9737658285ed017862e6677350e562ba4a2
amartya-rs <saha.amartya@outlook.com>
2023-11-27 15:17:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./components/Header/Header.jsx";
+import "./app.css";
+
+function App() {
+   return (
+      <div className="app">
+         <Header />
+      </div>
+   );
+}
+
+export { App };
